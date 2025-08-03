c0cae8f2198a9650bcdd6197c856a3ef682428f4
Drew Kedersha <drewkedersha@Drews-MacBook-Pro-2.local>
2025-08-03 14:09:19 -0500

    Fix: working Sanity env variables

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from '@sanity/image-url'
 export const sanity = createClient({
   projectId: 'bvnhi2h5', // replace with your real project ID
   dataset: 'production',
-  useCdn: true,
+  useCdn: false,
   apiVersion: '2024-06-30',
 })
 
