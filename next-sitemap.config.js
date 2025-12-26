44673079fb7678a22b91c5037ecb15772fc79890
drewkedersha <drewkedersha@gmail.com>
2025-12-26 13:16:55 -0600

    Use SANITY_* env vars for sitemap post URLs

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,35 @@
 /** @type {import('next-sitemap').IConfig} */
+
+const { createClient } = require("@sanity/client");
+
+const sanity = createClient({
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  apiVersion: process.env.SANITY_API_VERSION || "2025-01-01",
+  useCdn: true,
+});
+
 module.exports = {
-  siteUrl: 'https://www.newwavechristian.org',
-  generateRobotsTxt: false,
+  siteUrl: "https://www.newwavechristian.org",
+  generateRobotsTxt: true,
   sitemapSize: 7000,
-  changefreq: 'weekly',
+  changefreq: "weekly",
   priority: 0.7,
+  trailingSlash: false,
+
+  additionalPaths: async () => {
+    const posts = await sanity.fetch(`
+      *[_type == "post" && defined(slug.current)]{
+        "slug": slug.current,
+        _updatedAt
+      }
+    `);
+
+    return posts.map((p) => ({
+      loc: `/articles/${p.slug}`,
+      lastmod: p._updatedAt,
+      changefreq: "weekly",
+      priority: 0.8,
+    }));
+  },
 };
